d446a5529eb413ad38b9640122b58fa35ad856cc
Xiao <44850160+shixiao9941@users.noreply.github.com>
2023-02-13 03:27:40 -0600

    Create setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,10 @@
+conda create -n graphnarrator python=3.6
+conda activate graphnarrator
+pip install 'transformers==3.3.1'
+pip install 'pytorch-lightning==0.9.0'
+pip install 'torch==1.4.0'
+pip install 'parsimonious==0.8.1'
+pip install unidecode
+pip install rouge-score
+pip install gitpython
+pip install sacrebleu
